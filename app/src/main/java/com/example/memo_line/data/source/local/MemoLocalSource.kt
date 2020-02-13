968f11d770c7786d30553f775f9fe2531a8076c3
wonseok Choi <ws941220@gmail.com>
2020-02-13 17:56:05 +0900

    2020.02.13

--- app/src/main/java/com/example/memo_line/data/source/local/MemoLocalSource.kt
+++ app/src/main/java/com/example/memo_line/data/source/local/MemoLocalSource.kt
@@ -1,13 +1,35 @@
 package com.example.memo_line.data.source.local
 
+import androidx.annotation.VisibleForTesting
+import com.example.android.architecture.blueprints.todoapp.util.AppExecutors
 import com.example.memo_line.data.Memo
 import com.example.memo_line.data.source.MemosDataSource
 
-class TasksLocalDataSource private constructor(
+class MemoLocalDataSource private constructor(
+    val appExecutors: AppExecutors,
     val memoDao: MemoDao
 ) : MemosDataSource {
 
+    companion object {
+        private var INSTANCE: MemoLocalDataSource? = null
+
+        @JvmStatic
+        fun getInstance(appExecutors: AppExecutors, memoDao: MemoDao): MemoLocalDataSource {
+            if (INSTANCE == null) {
+                synchronized(MemoLocalDataSource::javaClass) {
+                    INSTANCE = MemoLocalDataSource(appExecutors, memoDao)
+                }
+            }
+            return INSTANCE!!
+        }
+
+        @VisibleForTesting
+        fun clearInstance() {
+            INSTANCE = null
+        }
+    }
+
     override fun saveMemo(memo: Memo) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        appExecutors.diskIO.execute { memoDao.insertMemo(memo) }
     }
 }
